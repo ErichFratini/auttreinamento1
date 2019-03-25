a78baac90ac28b3582b39b140d79e25ccef2c386
Erich Alves Fratini <erich.fratini.1998@gmail.com>
2019-03-25 16:56:23 -0300

    att

--- src/test/java/com/treinamento/support/Web.java
+++ src/test/java/com/treinamento/support/Web.java
@@ -8,11 +8,13 @@ import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
 public class Web {
-	//ChromeOptions options = new ChromeOptions();
+	static ChromeOptions options = new ChromeOptions();
 	//options.setBinary("/src/main/resources/drivers/chromedriver");
-   public static final String BINARY_CHROME_DRIVER = Paths.get("").toAbsolutePath().toString() + "/src/main/resources/drivers/chromedriver";
+    public static final String BINARY_CHROME_DRIVER = Paths.get("").toAbsolutePath().toString() + "/src/main/resources/drivers/chromedriver";
 
     public static WebDriver getChromeDriver() {
+    	options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         System.setProperty("webdriver.chrome.driver", BINARY_CHROME_DRIVER);
         WebDriver navegador = new ChromeDriver();
         navegador.manage().window().maximize();
