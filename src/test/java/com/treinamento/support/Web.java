8c93fbb993472a0f4ceb86ac1f7dc690d66cf40b
Erich Alves Fratini <erich.fratini.1998@gmail.com>
2019-03-26 08:30:10 -0300

    att

--- src/test/java/com/treinamento/support/Web.java
+++ src/test/java/com/treinamento/support/Web.java
@@ -16,15 +16,15 @@ public class Web {
     	//options.setBinary("/src/main/resources/drivers/chromedriver");
         System.setProperty("webdriver.chrome.driver", BINARY_CHROME_DRIVER);
     	//options.addArguments("--headless");
-    	options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("start-maximized"); // open Browser in maximized mode
         options.addArguments("disable-infobars"); // disabling infobars
         options.addArguments("--disable-extensions"); // disabling extensions
         options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
         WebDriver navegador = new ChromeDriver(options);
-        navegador.manage().window().maximize();
-        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //navegador.manage().window().maximize();
+        //navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         return navegador;
     }
 
