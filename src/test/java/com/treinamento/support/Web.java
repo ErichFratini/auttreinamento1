dc9c2b83c9f80938386d9db91a334632b2f47c68
Erich Alves Fratini <erich.fratini.1998@gmail.com>
2019-03-26 08:17:30 -0300

    att

--- src/test/java/com/treinamento/support/Web.java
+++ src/test/java/com/treinamento/support/Web.java
@@ -8,17 +8,21 @@ import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
 public class Web {
-	static ChromeOptions options = new ChromeOptions();
+
 	//
     public static final String BINARY_CHROME_DRIVER = "/home/erich/Downloads/drivers/chromedriver";
-
+    public static ChromeOptions options = new ChromeOptions();
     public static WebDriver getChromeDriver() {
     	//options.setBinary("/src/main/resources/drivers/chromedriver");
-    	options.addArguments("--headless");
+        System.setProperty("webdriver.chrome.driver", BINARY_CHROME_DRIVER);
+    	//options.addArguments("--headless");
     	options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        System.setProperty("webdriver.chrome.driver", BINARY_CHROME_DRIVER);
-        WebDriver navegador = new ChromeDriver();
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        WebDriver navegador = new ChromeDriver(options);
         navegador.manage().window().maximize();
         navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         return navegador;
